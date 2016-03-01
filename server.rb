a8d10c13f3651f0085a0b61cb593f86d28f47eaf
Dan Garland <dan@dangarland.co.uk>
2016-03-01 13:43:43 +0000

    WIP on getting files in and out of mongo

--- server.rb
+++ server.rb
@@ -1,29 +1,57 @@
 require 'sinatra'
-require "sinatra/streaming"
-require 'mongoid/grid_fs'
+require 'sinatra/streaming'
+require 'mongo'
+require 'pry'
 
-configure do
-  Mongoid.load!('config/mongoid.yml', :development)
-end
+set :client, Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'videos')
 
 post '/video' do
-  grid_fs = Mongoid::GridFs
-  f = grid_fs.put(File.open(params[:video_path]))
+  video = File.open(params[:video_path])
+  file = Mongo::Grid::File.new(video.read, :filename => 'video.mp4')
+  video_id = settings.client.database.fs(:fs_name => 'videos').insert_one(file)
+
   content_type :json
-  [200, {}, { video_id: f.id} ]
+  [200, {}, { video_id: video_id} ]
 end
 
 get '/video/:id' do
-  grid_fs = Mongoid::GridFs
-  g = grid_fs.get(params[:id])
-  # content_type 'video/mp4'
-  # g.data
-  stream do |out|
-    g.each do |chunk|
-      out.puts(chunk.length)
-      out.flush
-    end
+  content_type 'video/mp4'
+
+  begin
+    fs_bucket = settings.client.database.fs(:fs_name => 'videos')
+    file = Tempfile.new('video')
+    fs_bucket.download_to_stream(BSON::ObjectId(params[:id]), file)
+  rescue Mongo::Error::UnexpectedChunkLength => e
+    puts e.message
+  rescue Mongo::Error::ClosedStream => e
+    puts e.message
   end
+
+  binding.pry
+
+  [200, {}, file.read]
+
+
+  # stream do |out|
+  #   unless out.closed?
+  #     begin
+  #       fs_bucket.open_download_stream(BSON::ObjectId(params[:id])) do |video_stream|
+  #         video_stream.each do |chunk|
+  #           out.write chunk
+  #         end
+  #         out.flush
+  #       end
+  #     rescue Mongo::Error::UnexpectedChunkLength => e
+  #       puts e.message
+  #       out.close
+  #     rescue Mongo::Error::ClosedStream => e
+  #       puts e.message
+  #       out.close
+  #     end
+  #   end
+  # end
+
+  # 200
 end
 
 

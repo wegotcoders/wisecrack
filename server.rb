c687bcf238f5019fadb7c57881f12ca1b0341892
Dan Garland <dan@dangarland.co.uk>
2016-03-02 12:47:34 +0000

    Got mp4 progresive downloads working

--- server.rb
+++ server.rb
@@ -5,7 +5,7 @@ require 'pry'
 
 set :client, Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'videos')
 
-post '/video' do
+post '/videos' do
   video = File.open(params[:video_path])
 
   video_id = settings.client.database.fs.upload_from_stream('video.mp4', video, { chunk_size: 10240000 })
@@ -15,12 +15,22 @@ post '/video' do
   # video_id = settings.client.database.fs(:fs_name => 'videos').insert_one(file)
 
   content_type :json
-  [200, {}, { video_id: video_id} ]
+  [201, {}, { video_id: video_id} ]
 end
 
-get '/video/:id' do
+get '/videos/show.html' do
+  erb :show
+end
+
+get '/videos/:id.:format' do
+  # TODO - Make this dynamic based on the filetype in gridfs..
   content_type 'video/mp4'
 
+  # TODO - Make this dynamic based on the size of the file in gridfs
+  headers \
+    'Content-Length' => '60317938'
+
+  # TODO - Let's use videos as the fs_name
   fs_bucket = settings.client.database.fs(:fs_name => 'fs')
 
   stream do |out|
@@ -29,7 +39,9 @@ get '/video/:id' do
         fs_bucket.open_download_stream(BSON::ObjectId(params[:id])) do |video_stream|
           video_stream.each do |chunk|
             out << chunk
-            sleep 0.5
+            # TODO - How best to regulate content? I'd like to be able to set
+            # bitrate to say 256k, 512k etc.
+            sleep 10
             out.flush
           end
         end

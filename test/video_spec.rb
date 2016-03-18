7424a2d1e3346b5c3586434cf2b36dfb49e3c654
Alfred Oliver Willder <alfred.oliver.willder@gmail.com>
2016-03-18 20:04:45 +0000

    1. test the error message for Wisecrack::Video::UploadError 2. test .stream_url

--- test/video_spec.rb
+++ test/video_spec.rb
@@ -2,13 +2,15 @@ require 'test_helper'
 
 describe Wisecrack::Video do
   describe 'class methods' do
+    before do
+      Wisecrack.config(:development) do |config|
+        config.base_host_url = 'http://localhost:9292'
+      end
+    end
+
     describe '.create(video_path)' do
       describe 'when request is succesful' do
         before do
-          Wisecrack.config(:development) do |config|
-            config.base_host_url = 'http://localhost:9292'
-          end
-
           VCR.insert_cassette('wisecrack_video_201')
 
           @video_id = Wisecrack::Video.create('test/fixtures/mock_upload_file.mp4')
@@ -32,10 +34,6 @@ describe Wisecrack::Video do
 
       describe 'when request is unsuccessful' do
         before do
-          Wisecrack.config(:development) do |config|
-            config.base_host_url = 'http://localhost:9292'
-          end
-
           VCR.insert_cassette('test/cassettes/wisecrack_video_404')
         end
 
@@ -43,9 +41,34 @@ describe Wisecrack::Video do
           VCR.eject_cassette
         end
 
-        it 'raises an UploadError' do
-          expect { Wisecrack::Video.create('file/that/does/not/exist.mp4') }.must_raise(Wisecrack::Video::UploadError)
+        describe 'the exception raised' do
+          it 'is an UploadError' do
+            upload_error = assert_raises(Wisecrack::Video::UploadError) do
+              Wisecrack::Video.create('file/that/does/not/exist.mp4')
+            end
+          end
+
+          it 'contains a message for the devleloper' do
+            path_that_does_not_exist = 'file/that/does/not/exist.mp4'
+
+            upload_error = assert_raises(Wisecrack::Video::UploadError) do
+              Wisecrack::Video.create(path_that_does_not_exist)
+            end
+
+            upload_error.message.must_equal "Failed to create with video #{path_that_does_not_exist}"
+          end
         end
+      end
+    end
+
+    describe('.stream_url') do
+      it 'returns a correctly formatted url' do
+        mock_mongo_grid_fs_id = 'mock_id'
+        mock_bit_rate = 999
+        mock_file_extension = 'xyz'
+
+        stream_url = Wisecrack::Video.stream_url(mongo_grid_fs_id: mock_mongo_grid_fs_id, bit_rate: mock_bit_rate, video_encoding: mock_file_extension)
+        stream_url.must_equal('http://localhost:9292/videos/mock_id-999.xyz')
       end
     end
   end

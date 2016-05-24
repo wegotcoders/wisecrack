8ed2547b0b740e21f8260d883e2cb05d6e3842fd
Alfred Oliver Willder <alfred.oliver.willder@gmail.com>
2016-05-24 19:09:15 +0100

    adding simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require 'simplecov'
+SimpleCov.start
 require 'wisecrack/api'
 require 'rack/test'
 require 'webmock'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,8 @@
+require 'simplecov'
+SimpleCov.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'wisecrack'
 require 'rack/test'
 require 'minitest/autorun'
+require 'pry'
